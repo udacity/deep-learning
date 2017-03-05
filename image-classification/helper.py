ce2715f81c233132e6ff22b4e19fc5d518018af2
Ritesh Malaiya <ritesh.malaiya@gmail.com>
2017-03-05 21:30:39 +0000

    Code completed with basic network

--- image-classification/helper.py
+++ image-classification/helper.py
@@ -53,8 +53,9 @@ def display_stats(cifar10_dataset_folder_path, batch_id, sample_id):
     print('Image - Min Value: {} Max Value: {}'.format(sample_image.min(), sample_image.max()))
     print('Image - Shape: {}'.format(sample_image.shape))
     print('Label - Label Id: {} Name: {}'.format(sample_label, label_names[sample_label]))
+    plt.subplots()
     plt.axis('off')
-    plt.imshow(sample_image)
+    #plt.imshow(sample_image)
 
 
 def _preprocess_and_save(normalize, one_hot_encode, features, labels, filename):
