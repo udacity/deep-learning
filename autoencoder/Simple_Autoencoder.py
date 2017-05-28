1e5510b2766652c9231414e2924fe4ce0dbe7c18
Ewan Nicolson <ewan.nicolson@skyscanner.net>
2017-05-28 15:05:18 +0100

    autoencoder exercises

---
+++ autoencoder/Simple_Autoencoder.py
@@ -0,0 +1,116 @@
+
+# coding: utf-8
+
+# # A Simple Autoencoder
+# 
+# We'll start off by building a simple autoencoder to compress the MNIST dataset. With autoencoders, we pass input data through an encoder that makes a compressed representation of the input. Then, this representation is passed through a decoder to reconstruct the input data. Generally the encoder and decoder will be built with neural networks, then trained on example data.
+# 
+# ![Autoencoder](assets/autoencoder_1.png)
+# 
+# In this notebook, we'll be build a simple network architecture for the encoder and decoder. Let's get started by importing our libraries and getting the dataset.
+
+
+get_ipython().magic('matplotlib inline')
+
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('MNIST_data', validation_size=0)
+
+
+# Below I'm plotting an example image from the MNIST dataset. These are 28x28 grayscale images of handwritten digits.
+
+
+img = mnist.train.images[2]
+plt.imshow(img.reshape((28, 28)), cmap='Greys_r')
+
+
+# We'll train an autoencoder with these images by flattening them into 784 length vectors. The images from this dataset are already normalized such that the values are between 0 and 1. Let's start by building basically the simplest autoencoder with a **single ReLU hidden layer**. This layer will be used as the compressed representation. Then, the encoder is the input layer and the hidden layer. The decoder is the hidden layer and the output layer. Since the images are normalized between 0 and 1, we need to use a **sigmoid activation on the output layer** to get values matching the input.
+# 
+# ![Autoencoder architecture](assets/simple_autoencoder.png)
+# 
+# 
+# > **Exercise:** Build the graph for the autoencoder in the cell below. The input images will be flattened into 784 length vectors. The targets are the same as the inputs. And there should be one hidden layer with a ReLU activation and an output layer with a sigmoid activation. The loss should be calculated with the cross-entropy loss, there is a convenient TensorFlow function for this `tf.nn.sigmoid_cross_entropy_with_logits` ([documentation](https://www.tensorflow.org/api_docs/python/tf/nn/sigmoid_cross_entropy_with_logits)). You should note that `tf.nn.sigmoid_cross_entropy_with_logits` takes the logits, but to get the reconstructed images you'll need to pass the logits through the sigmoid function.
+
+
+# Size of the encoding layer (the hidden layer)
+encoding_dim = 32 # feel free to change this value
+
+image_size = mnist.train.images.shape[1]
+
+inputs_ = tf.placeholder(tf.float32, (None, image_size), name='inputs')
+targets_ = tf.placeholder(tf.float32, (None, image_size), name='targets')
+
+# Output of hidden layer
+encoded = tf.layers.dense(inputs_, encoding_dim, activation=tf.nn.relu)
+
+# Output layer logits
+logits = tf.layers.dense(encoded, image_size, activation=None)
+# Sigmoid output from logits
+decoded = tf.nn.sigmoid(logits, name='output')
+
+# Sigmoid cross-entropy loss
+loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=targets_, logits = logits)
+# Mean of the loss
+cost = tf.reduce_mean(loss)
+
+# Adam optimizer
+opt = tf.train.AdamOptimizer(0.001).minimize(cost)
+
+
+# ## Training
+
+
+# Create the session
+sess = tf.Session()
+
+
+# Here I'll write a bit of code to train the network. I'm not too interested in validation here, so I'll just monitor the training loss. 
+# 
+# Calling `mnist.train.next_batch(batch_size)` will return a tuple of `(images, labels)`. We're not concerned with the labels here, we just need the images. Otherwise this is pretty straightfoward training with TensorFlow. We initialize the variables with `sess.run(tf.global_variables_initializer())`. Then, run the optimizer and get the loss with `batch_cost, _ = sess.run([cost, opt], feed_dict=feed)`.
+
+
+epochs = 20
+batch_size = 200
+sess.run(tf.global_variables_initializer())
+for e in range(epochs):
+    for ii in range(mnist.train.num_examples//batch_size):
+        batch = mnist.train.next_batch(batch_size)
+        feed = {inputs_: batch[0], targets_: batch[0]}
+        batch_cost, _ = sess.run([cost, opt], feed_dict=feed)
+
+        print("Epoch: {}/{}...".format(e+1, epochs),
+              "Training loss: {:.4f}".format(batch_cost))
+
+
+# ## Checking out the results
+# 
+# Below I've plotted some of the test images along with their reconstructions. For the most part these look pretty good except for some blurriness in some parts.
+
+
+fig, axes = plt.subplots(nrows=2, ncols=10, sharex=True, sharey=True, figsize=(20,4))
+in_imgs = mnist.test.images[:10]
+reconstructed, compressed = sess.run([decoded, encoded], feed_dict={inputs_: in_imgs})
+
+for images, row in zip([in_imgs, reconstructed], axes):
+    for img, ax in zip(images, row):
+        ax.imshow(img.reshape((28, 28)), cmap='Greys_r')
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+fig.tight_layout(pad=0.1)
+
+
+
+sess.close()
+
+
+# ## Up Next
+# 
+# We're dealing with images here, so we can (usually) get better performance using convolution layers. So, next we'll build a better autoencoder with convolutional layers.
+# 
+# In practice, autoencoders aren't actually better at compression compared to typical methods like JPEGs and MP3s. But, they are being used for noise reduction, which you'll also build.
