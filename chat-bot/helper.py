f1fa55e0e49377dc9f405bd4c0b26e6389436600
Thalles Silva <thalles753@gmail.com>
2017-06-22 10:54:46 -0300

    chatbot

--- chat-bot/helper.py
+++ chat-bot/helper.py
@@ -12,7 +12,7 @@ def load_data(path):
     Load Dataset from File
     """
     input_file = os.path.join(path)
-    with open(input_file, 'r', encoding='utf-8') as f:
+    with open(input_file, "r", encoding='utf-8', errors='ignore') as f:
         data = f.read()
 
     return data
