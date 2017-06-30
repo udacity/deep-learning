95f11682c4b9473a6393eabe3394fd8bc4adddc6
J Alammar <alammar@gmail.com>
2017-07-01 00:20:19 +0300

    Update problem_unittests.py

--- language-translation/problem_unittests.py
+++ language-translation/problem_unittests.py
@@ -194,7 +194,7 @@ def test_decoding_layer(decoding_layer):
             'Found wrong type: {}'.format(type(infer_logits_output))
 
         assert train_decoder_output.rnn_output.get_shape().as_list() == [batch_size, None, vocab_size], \
-            'Wrong shape returned.  Found {}'.format(train_decoder_output.get_shape())
+            'Wrong shape returned.  Found {}'.format(train_decoder_output.rnn_output.get_shape())
         assert infer_logits_output.sample_id.get_shape().as_list() == [batch_size, None], \
              'Wrong shape returned.  Found {}'.format(infer_logits_output.sample_id.get_shape())
 
@@ -253,7 +253,7 @@ def test_seq2seq_model(seq2seq_model):
             'Found wrong type: {}'.format(type(infer_logits_output))
 
         assert train_decoder_output.rnn_output.get_shape().as_list() == [batch_size, None, vocab_size], \
-            'Wrong shape returned.  Found {}'.format(train_decoder_output.get_shape())
+            'Wrong shape returned.  Found {}'.format(train_decoder_output.rnn_output.get_shape())
         assert infer_logits_output.sample_id.get_shape().as_list() == [batch_size, None], \
              'Wrong shape returned.  Found {}'.format(infer_logits_output.sample_id.get_shape())
 
