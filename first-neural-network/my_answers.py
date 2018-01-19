d7a11fdb9ead326be211f327edce4a7e2cf99a98
Unnat Singh <unnatsingh5@gmail.com>
2018-01-19 18:41:33 +0530

    My_neural_network(Non_Vectorized)

--- first-neural-network/my_answers.py
+++ first-neural-network/my_answers.py
@@ -84,24 +84,24 @@ class NeuralNetwork(object):
             delta_weights_h_o: change in weights from hidden to output layers
 
         '''
-        #### Implement the backward pass here ####
+         #### Implement the backward pass here ####
         ### Backward pass ###
 
         # TODO: Output error - Replace this value with your calculations.
-        error = None # Output layer error is the difference between desired target and actual output.
+        error = y - final_outputs # Output layer error is the difference between desired target and actual output.
         
         # TODO: Calculate the hidden layer's contribution to the error
-        hidden_error = None
+        hidden_error = error*hidden_outputs*(1-hidden_outputs)
         
         # TODO: Backpropagated error terms - Replace these values with your calculations.
-        output_error_term = None
+        output_error_term = error*hidden_outputs
         
-        hidden_error_term = None
+        hidden_error_term = hidden_error*X[:,None]*self.weights_hidden_to_output.T
         
         # Weight step (input to hidden)
-        delta_weights_i_h += None
+        delta_weights_i_h += hidden_error_term
         # Weight step (hidden to output)
-        delta_weights_h_o += None
+        delta_weights_h_o += output_error_term[:,None]
         return delta_weights_i_h, delta_weights_h_o
 
     def update_weights(self, delta_weights_i_h, delta_weights_h_o, n_records):
@@ -114,8 +114,8 @@ class NeuralNetwork(object):
             n_records: number of records
 
         '''
-        self.weights_hidden_to_output += None # update hidden-to-output weights with gradient descent step
-        self.weights_input_to_hidden += None # update input-to-hidden weights with gradient descent step
+        self.weights_hidden_to_output += (self.lr *delta_weights_h_o)/n_records # update hidden-to-output weights with gradient descent step
+        self.weights_input_to_hidden += (self.lr *delta_weights_i_h)/n_records # update input-to-hidden weights with gradient descent step
 
     def run(self, features):
         ''' Run a forward pass through the network with input features 
