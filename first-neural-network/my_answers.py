704bbdcf67cb0744257716bc36f5bd6cd0e5d0a6
Unnat Singh <unnatsingh5@gmail.com>
2018-01-19 18:19:38 +0530

    Vectorized approach

--- first-neural-network/my_answers.py
+++ first-neural-network/my_answers.py
@@ -44,11 +44,11 @@ class NeuralNetwork(object):
         n_records = features.shape[0]
         delta_weights_i_h = np.zeros(self.weights_input_to_hidden.shape)
         delta_weights_h_o = np.zeros(self.weights_hidden_to_output.shape)
-        for X, y in zip(features, targets):
-            
-            final_outputs, hidden_outputs = self.forward_pass_train(X)  # Implement the forward pass function below
+        X=features
+        y=targets    
+        final_outputs, hidden_outputs = self.forward_pass_train(X)  # Implement the forward pass function below
             # Implement the backproagation function below
-            delta_weights_i_h, delta_weights_h_o = self.backpropagation(final_outputs, hidden_outputs, X, y,delta_weights_i_h, delta_weights_h_o)
+        delta_weights_i_h, delta_weights_h_o = self.backpropagation(final_outputs, hidden_outputs, X, y,delta_weights_i_h, delta_weights_h_o)
         self.update_weights(delta_weights_i_h, delta_weights_h_o, n_records)
 
 
@@ -92,14 +92,14 @@ class NeuralNetwork(object):
         # TODO: Calculate the hidden layer's contribution to the error
         hidden_error = error*hidden_outputs*(1-hidden_outputs)
         # TODO: Backpropagated error terms - Replace these values with your calculations.
-        output_error_term = error*hidden_outputs
+        output_error_term = np.dot(hidden_outputs.T,error)
         
-        hidden_error_term = hidden_error*X[:,None]*self.weights_hidden_to_output.T
+        hidden_error_term =np.dot(X.T,hidden_error)*self.weights_hidden_to_output.T
         
         # Weight step (input to hidden)
-        delta_weights_i_h += hidden_error_term
+        delta_weights_i_h = hidden_error_term
         # Weight step (hidden to output)
-        delta_weights_h_o += output_error_term[:,None]
+        delta_weights_h_o = output_error_term
         return delta_weights_i_h, delta_weights_h_o
 
     def update_weights(self, delta_weights_i_h, delta_weights_h_o, n_records):
